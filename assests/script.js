b2ae8e832357d0baf2b9c8c2b1f4f924f95db5ad
unname666 <78314985+unname666@users.noreply.github.com>
2021-03-02 08:26:32 -0700

    getting started

---
+++ assests/script.js
@@ -0,0 +1,35 @@
+// to build a quiz
+// i need questions
+var myQuestions = [
+	{
+		question: "What is 10/2?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '115'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "What is 30/3?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '10'
+		},
+		correctAnswer: 'c'
+	},
+    {
+        question: "what is 20-5"
+        answers: {
+            a: '10',
+            b: '15',
+            c: '30'
+        },
+        correctAnswer: 'c'
+    }
+];
+//i need a timer
+//i need to keep score
+// i need to track the score
+// i need the sumbit button to work
