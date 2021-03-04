97a87140eb0d0919c1fec5f018adaf21c7dcc5f3
unname666 <78314985+unname666@users.noreply.github.com>
2021-03-03 21:19:56 -0700

    getting there

---
+++ assests/js/script.js
@@ -0,0 +1,77 @@
+var $startPrompt = document.querySelector("#start-prompt");
+var $startBtn = document.querySelector("#start-prompt button");
+var $questionPrompt = document.querySelector("#question-prompt");
+var $questionText = document.querySelector("#question-text");
+var $questionOptions = document.querySelector("#options");
+var $timer = document.querySelector("#timer");
+
+
+var secondsLeft = 10;
+
+
+
+// i need questions for the quiz
+var questions = [
+  {
+    text: "What is 10/2?",
+    options: ["3", "5", "115"],
+    correctAnswer: "5",
+  },
+  {
+    text: "What is 30/3?",
+    options: ["3", "5", "10"],
+    correctAnswer: "10",
+  },
+  {
+    text: "what is 20-5",
+    options: ["10", "15", "30"],
+    correctAnswer: "30",
+  },
+  {
+    text: "Are you awesome?",
+    options: ["true", "False"],
+    correctAnswer: "True",
+  },
+];
+
+$startBtn.addEventListener("click", startQuestions);
+
+
+function startQuestions() {
+	  //hide the start prompt
+  $startPrompt.classList.add("hide");
+  //show our question
+  $questionPrompt.classList.remove("hide");
+  
+  	//update the content with current question
+  	$questionText.textContent = questions[0].text;
+  	//render options
+ 	 questions[0].options.forEach(function (item) {
+    var $btn = document.createElement("button");
+    $btn.textContent = item;
+    options.addEventListener("click", answers)
+    $questionOptions.append($btn);
+    //display our timer
+    startTime();
+  });
+}
+
+function answers(e){
+  $questionOptions = e.target;
+  if ($questionOptions.textContent === questions[0].correctAnswer){
+    console.log("it worked")
+  }
+
+}
+
+function startTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    $timer.textContent = secondsLeft;
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
+
